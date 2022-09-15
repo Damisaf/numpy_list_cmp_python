3ccddb75aaafa8c4b9cc9c0f2a4f9365a0aa2ca6
Damisaf <109141323+Damisaf@users.noreply.github.com>
2022-09-14 22:44:58 -0300

    Add files via upload

--- ejercicios_profundizacion/profundizacion_3.py
+++ ejercicios_profundizacion/profundizacion_3.py
@@ -62,5 +62,8 @@ if __name__ == '__main__':
     # A partir de aquí escriba el código que resuelve el enunciado
     # Leer el enunciado con atención y consultar cualquier duda
 
+    lista_compra_producto = [producto[x] if producto.get(x) != None else "NaN" for x in lista_compra_id]
+    print(lista_compra_producto)
+
 
     print("terminamos")
